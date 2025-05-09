1a3809b4c89c784fa8606e0323a5ebfeafa78968
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2025-05-08 22:46:46 -0300

    Change highlighting to match VSCode

--- languages/vue/highlights.scm
+++ languages/vue/highlights.scm
@@ -4,12 +4,19 @@
 (interpolation) @punctuation.special
 (raw_text) @embedded
 
+; This rule captures tag names starting with an uppercase for components
 ((tag_name) @type
  (#match? @type "^[A-Z]"))
+
+((tag_name) @tag
+ (#not-match? @tag "^[A-Z]"))
 
 (directive_name) @keyword
 (directive_argument) @constant
 
-(start_tag) @tag
-(end_tag) @tag
-(self_closing_tag) @tag
+; Adjust the captures only for brackets, not entire tags
+((start_tag "<" @punctuation.bracket)
+ (end_tag "</" @punctuation.bracket)
+ (self_closing_tag "<" @punctuation.bracket "/>")
+ (self_closing_tag "/>" @punctuation.bracket))
+
